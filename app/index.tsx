1b5e5bae791dc62e0eec2f3fced117019cad7d2a
rbeck19 <ryan.beck219@gmail.com>
2024-08-30 10:42:23 -0400

    app set up

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
